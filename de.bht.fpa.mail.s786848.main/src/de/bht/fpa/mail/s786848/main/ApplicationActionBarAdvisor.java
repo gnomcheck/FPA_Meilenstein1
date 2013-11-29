eb63e795cc572e714b397b08627885572a20f162
Chris <chris.hock86@gmail.com>
2013-11-29 10:27:20 +0100

    new comitten

--- de.bht.fpa.mail.s786848.main/src/de/bht/fpa/mail/s786848/main/ApplicationActionBarAdvisor.java
+++ de.bht.fpa.mail.s786848.main/src/de/bht/fpa/mail/s786848/main/ApplicationActionBarAdvisor.java
@@ -7,14 +7,16 @@ import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
-    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
-        super(configurer);
-    }
+  public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+    super(configurer);
+  }
 
-    protected void makeActions(IWorkbenchWindow window) {
-    }
+  @Override
+  protected void makeActions(IWorkbenchWindow window) {
+  }
 
-    protected void fillMenuBar(IMenuManager menuBar) {
-    }
-    
+  @Override
+  protected void fillMenuBar(IMenuManager menuBar) {
+  }
+
 }
--- de.bht.fpa.mail.s786848.main/src/de/bht/fpa/mail/s786848/main/ApplicationWorkbenchAdvisor.java
+++ de.bht.fpa.mail.s786848.main/src/de/bht/fpa/mail/s786848/main/ApplicationWorkbenchAdvisor.java
@@ -6,13 +6,15 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String PERSPECTIVE_ID = "de.bht.fpa.mail.s786848.main.perspective"; //$NON-NLS-1$
+  private static final String PERSPECTIVE_ID = "de.bht.fpa.mail.s786848.main.perspective"; //$NON-NLS-1$
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
+  @Override
+  public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+    return new ApplicationWorkbenchWindowAdvisor(configurer);
+  }
 
-	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
-	}
+  @Override
+  public String getInitialWindowPerspectiveId() {
+    return PERSPECTIVE_ID;
+  }
 }
